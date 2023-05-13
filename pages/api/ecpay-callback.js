4271748e9d483af5ce252708b9522795cb32bdb0
a93701011 <a93701011@gmial.com>
2023-05-13 17:40:22 +0800

    ecpay

--- pages/api/ecpay-callback.js
+++ pages/api/ecpay-callback.js
@@ -1,34 +1,20 @@
-// 綠界 API 商店相關參數
+
+const sql = require('mssql');
+import config from '../../config/config';
+const pool = new sql.ConnectionPool(config);
+
 const MERCHANT_ID = process.env.MERCHANT_ID;
 const HASH_KEY = process.env.HASH_KEY;
 const HASH_IV = process.env.HASH_IV;
-import config from '../../config/config';
-const pool = new sql.ConnectionPool(config);
 
 export default async function ecpaycallback(req, res) {
   if (req.method === 'POST') {
 
     const data = req.body
     const getCheckMacValue = computeCheckMacValue(data);
-    const MerchantTradeNo = data.MerchantTradeNo
-    const RtnCode = data.RtnCode
-    const RtnMsg = data.RtnMsg
     const checkMacValue = data.CheckMacValue
-
-
-
     if (checkMacValue == getCheckMacValue) {
-        await pool.connect();
-        const request = new sql.Request(pool);
-        request.input('MerchantTradeNo', sql.VarChar, MerchantTradeNo);
-        request.input('RtnCode', sql.VarChar, RtnCode);
-        request.input('RtnMsg', sql.VarChar, RtnMsg);
-        request.input('ecpay_callback_datetime', sql.DateTime, new Date());
-        const result = await request.query(`
-      INSERT INTO [accentcoach_epaycallback] (MerchantTradeNo, RtnCode, RtnMsg, ecpay_callback_datetime)
-      VALUES ( @MerchantTradeNo, @RtnCode, @RtnMsg, @ecpay_callback_datetime )
-      `);
-      await pool.close();    
+        
       res.status(200).send('1|OK')
     } else {
       res.status(400).send('0|FAIL')
